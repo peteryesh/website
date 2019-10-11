3cfbfac6041ccbb387eb287ba4e1136170c6207c
Peter Noh <petercnoh@gmail.com>
2019-10-10 21:22:31 -0700

    added routing pt2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {HashRouter as Router} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
     return (
